9f152a70b8a5d38f0e71c16046856e283a62e79b
AbanoubHM <abanoubharby33@gmail.com>
2022-03-06 02:48:15 +0200

    image url with interpolation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
-
+  title:string="Home";
+  imgUrl:string="assets/home-bg.jpg";
   ngOnInit(): void {
   }
 
